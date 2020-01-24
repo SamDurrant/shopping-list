4e4555836699de6596747b26de4b83567b778068
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-01-24 09:19:00 -0700

    finished file and folder structure

---
+++ src/components/GroceryItem/GroceryItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class GroceryItem extends Component {
+  render() {
+    return (
+      <div>
+        <p>potatoes</p>
+      </div>
+    )
+  }
+}
+
+export default GroceryItem;
