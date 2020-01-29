093098313f451731767c793eeca641eff6ce57b0
Alex Morton <am@alexmortoncreative.com>
2020-01-29 11:33:40 +0100

    committing before pulling from master

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from './components/NavBar/NavBar';
+import Home from './pages/Home/Home';
+import Footer from './components/Footer/Footer';
 
 
-function App() {
-  return (
-    <>
-      <NavBar />
-      <Home />
-      <Footer />
-    </>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Shopping List Test</h1>
+      </div>
+    );
+  }
 }
+
 
 ReactDOM.render(<App/>, document.querySelector("#root"));
