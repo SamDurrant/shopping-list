d60898ec799f3bded6f34327b8e2aac8af347212
Samantha Durrant <samantha.durrant.87@gmail.com>
2020-01-29 13:39:01 -0700

    Organize meta content in index.html Add pineapple favicon Change css import and class styles in NavBar to match other components Set up root element in index.css for colors Add add-item button to dashboard Add clear-list button to dashboard Add state to Home component Add methods to Home component to handle input change, add item and clear list Pass props to GroceryList and Dashboard components so add and clear buttons work

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,12 +4,42 @@ import Dashboard from '../../components/Dashboard/Dashboard';
 import './Home.css';
 
 export class Home extends Component {
+  state = {
+    userItemInput: '',
+    items: ['bananas', 'apples', 'oatmeal', 'eggs', 'bread', 'chocolates']
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+
+  addItemToList = () => {
+    let allItems = [this.state.userItemInput, ...this.state.items];
+    this.setState({
+      userItemInput: '',
+      items: allItems
+    })
+  }
+
+  clearItemsFromList = () => {
+    this.setState({
+      items: []
+    })
+  }
+
   render() {
     return (
       <div className="home">
         <h3>Grocery List</h3>
-        <GroceryList />
-        <Dashboard />
+        <GroceryList 
+          allItems={this.state.items}/>
+        <Dashboard 
+          change={this.handleChange}
+          inputValue={this.state.userItemInput}
+          addItem={this.addItemToList}
+          clearList={this.clearItemsFromList}/>
       </div>
     )
   }
